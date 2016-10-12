afe40f346bf1097ba98b0c4fcc4a9e7df568c643
Michael Pardo <kensuke155@gmail.com>
2016-10-12 10:23:01 -0400

    Add hyper config.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 ln -sni $HOME/.shellrc/bash_profile $HOME/.bash_profile
 ln -sni $HOME/.shellrc/bashrc $HOME/.bashrc
 ln -sni $HOME/.shellrc/inputrc $HOME/.inputrc
+ln -sni $HOME/.shellrc/hyper.js $HOME/.hyper.js
