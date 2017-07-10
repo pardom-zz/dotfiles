cfcb9c2056cce7a7b57d2b795c1dd5ee127e6426
Michael Pardo <kensuke155@gmail.com>
2017-07-10 10:42:24 -0400

    Add Android tools path.

--- rc.d/paths.sh
+++ rc.d/paths.sh
@@ -6,5 +6,5 @@ export PATH=$PATH:$HOME/.bin
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:/usr/local/sbin
-export PATH=$PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools
 export PATH=$PATH:/Source/flutter/flutter/bin
